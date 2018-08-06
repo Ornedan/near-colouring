fb8793459e59d8b9e08353a6c371d2e600816da2
Heikki Aitakangas <ornedan@gmail.com>
2018-08-06 20:13:40 +0300

    Remove failed experimental export code

--- src/Lib/ImageExport.hs
+++ src/Lib/ImageExport.hs
@@ -16,48 +16,12 @@ exportGIFAnimation image order path = L.writeFile path encoded
         Left errMsg -> error errMsg
         Right bytes -> bytes
 
-    frames = pixelFrames
-    frames' = backFrame:pixelFrames
+    frames = VU.foldr (\coords rest -> toFrame coords : rest) [] order
 
-    backFrame :: (Int, Int, Maybe Palette, Maybe Int, GifDelay, DisposalMethod, Image Pixel8)
-    backFrame = let palette = generateImage (\_ _ -> PixelRGB8 0 0 0) 1 1
-                    pixels  = generateImage (\_ _ -> 0) (imageWidth image) (imageHeight image)
-                in (0, 0, Just palette, Just 0, 0, DisposalAny, pixels)
-
-    pixelFrames = VU.foldr (\coords rest -> toPixelFrame coords : rest) [] order
-
-    toPixelFrame :: (Int, Int) -> (Int, Int, Maybe Palette, Maybe Int, GifDelay, DisposalMethod, Image Pixel8)
-    toPixelFrame (x, y) = let palette = generateImage (\_ _ -> pixelRgbAt x y) 1 1
-                              pixels  = generateImage (\_ _ -> 0) 1 1
-                          in (x, y, Just palette, Nothing, 0, DisposalAny, pixels)
-
-    pixelRgbAt x y = let PixelRGBA8 r g b _ = pixelAt image x y
-                     in PixelRGB8 r g b
-
-exportGIFAnimation' :: Image PixelRGBA8 -> VU.Vector (Int, Int) -> FilePath -> IO ()
-exportGIFAnimation' image order path = L.writeFile path encoded
-  where
-    encoded =
-      case encodeComplexGifImage (imageWidth image) (imageHeight image) Nothing Nothing LoopingNever frames of
-        Left errMsg -> error errMsg
-        Right bytes -> bytes
-
-    frames = backFrame:pixelFrames
-    
-    backFrame = let palette = generateImage (\_ _ -> PixelRGB8 0 0 0) 1 1
-                    pixels  = generateImage (\_ _ -> 0) (imageWidth image) (imageHeight image)
-                in (0, 0, Just palette, Just 0, 0, DisposalAny, pixels)
-
-    pixelFrames = VU.foldr (\coords rest -> toPixelFrame coords : rest) [] order
-
-    toPixelFrame :: (Int, Int) -> (Int, Int, Maybe Palette, Maybe Int, GifDelay, DisposalMethod, Image Pixel8)
-    toPixelFrame (x, y) = let palette = generateImage paletteF 2 1
-                              paletteF 0 0 = PixelRGB8 0 0 0
-                              paletteF 1 0 = pixelRgbAt x y
-                              pixels = generateImage pixelsF (imageWidth image) (imageHeight image)
-                              pixelsF x' y' | x == x' && y == y' = 1
-                                            | otherwise          = 0
-                          in (0, 0, Just palette, Just 0, 0, DisposalDoNot, pixels)
+    toFrame :: (Int, Int) -> (Int, Int, Maybe Palette, Maybe Int, GifDelay, DisposalMethod, Image Pixel8)
+    toFrame (x, y) = let palette = generateImage (\_ _ -> pixelRgbAt x y) 1 1
+                         pixels  = generateImage (\_ _ -> 0) 1 1
+                     in (x, y, Just palette, Nothing, 0, DisposalAny, pixels)
 
     pixelRgbAt x y = let PixelRGBA8 r g b _ = pixelAt image x y
                      in PixelRGB8 r g b
